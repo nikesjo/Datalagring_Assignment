e5375b54bfb9edf8c8c8fa2e04881e77a67ecf9c
Nike <nikesjolin@outlook.com>
2024-01-31 14:36:44 +0100

    Created generic repository

---
+++ Infrastructure/Repositories/Repo.cs
@@ -0,0 +1,13 @@
+﻿using Infrastructure.Contexts;
+
+namespace Infrastructure.Repositories;
+
+public abstract class Repo<TEntity> where TEntity : class
+{
+    private readonly UserContext _userContext;
+
+    protected Repo(UserContext userContext)
+    {
+        _userContext = userContext;
+    }
+}
