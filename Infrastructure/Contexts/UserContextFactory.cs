ce69e6eec4d53a6d3284be2185ef2ddd3228a382
Nike <nikesjolin@outlook.com>
2024-01-31 12:01:12 +0100

    Created UserContextFactory

---
+++ Infrastructure/Contexts/UserContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Infrastructure.Contexts;
+
+public class UserContextFactory : IDesignTimeDbContextFactory<UserContext>
+{
+    public UserContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<UserContext>();
+        optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Education\datalagring\assignment\Datalagring_Assignment\Infrastructure\Data\user_database_cf.mdf;Integrated Security=True;Connect Timeout=30");
+
+        return new UserContext(optionsBuilder.Options);
+    }
+}
