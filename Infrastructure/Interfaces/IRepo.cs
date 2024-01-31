6b5043a449ef43d99f9055b0ebf51b3a99cfb6d2
Nike <nikesjolin@outlook.com>
2024-01-31 16:03:53 +0100

    Added interface to generic repository

---
+++ Infrastructure/Interfaces/IRepo.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Infrastructure.Interfaces;
+
+public interface IRepo<TEntity> where TEntity : class
+{
+    Task<TEntity> CreateAsync(TEntity entity);
+    Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> expression);
+    Task<IEnumerable<TEntity>> GetAsync();
+    Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> expression);
+    Task<TEntity> UpdateAsync(Expression<Func<TEntity, bool>> expression, TEntity entity);
+    Task<bool> DeleteAsync(Expression<Func<TEntity, bool>> expression);
+}
--- Infrastructure/Repositories/Repo.cs
+++ Infrastructure/Repositories/Repo.cs
@@ -1,13 +1,46 @@
 ﻿using Infrastructure.Contexts;
+using Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace Infrastructure.Repositories;
 
-public abstract class Repo<TEntity> where TEntity : class
+public abstract class Repo<TEntity, TContext> : IRepo<TEntity> where TEntity : class where TContext : DbContext
 {
-    private readonly UserContext _userContext;
+    private readonly TContext _context;
 
-    protected Repo(UserContext userContext)
+    protected Repo(TContext context)
     {
-        _userContext = userContext;
+        _context = context;
+    }
+
+    public Task<TEntity> CreateAsync(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(Expression<Func<TEntity, bool>> expression)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> expression)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<TEntity>> GetAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> expression)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TEntity> UpdateAsync(Expression<Func<TEntity, bool>> expression, TEntity entity)
+    {
+        throw new NotImplementedException();
     }
 }
