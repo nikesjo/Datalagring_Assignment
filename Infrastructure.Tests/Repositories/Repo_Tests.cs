50d953da19126ec186b17758688c06bfb696929a
Nike <nikesjolin@outlook.com>
2024-01-31 16:54:45 +0100

    not done

---
+++ Infrastructure.Tests/Repositories/Repo_Tests.cs
@@ -0,0 +1,10 @@
+﻿using Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Tests.Repositories;
+
+public class Repo_Tests<TContext> where TContext : DbContext, new()
+{
+    //private readonly TContext _context = new(new DbContextOptionsBuilder<TContext>().UseInMemoryDatabase($"{Guid.NewGuid()}").Options);
+
+}
