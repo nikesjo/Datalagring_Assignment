5b686c33b639cfeb7ad7c19edd1b88e29c01804b
Nike <nikesjolin@outlook.com>
2024-02-01 14:41:02 +0100

    Added dtos

---
+++ Infrastructure/Dtos/UserRegistrationDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Infrastructure.Dtos;
+
+public class UserRegistrationDto
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string StreetName { get; set; } = null!;
+    public string PostalCode { get; set; } = null!;
+    public string City { get; set; } = null!;
+}
