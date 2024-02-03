1e22438b304d709ec6669e7c2f648dcdac7baece
Nike <nikesjolin@outlook.com>
2024-02-03 16:33:24 +0100

    Changes done

--- Infrastructure/Entities/UserEntity.cs
+++ Infrastructure/Entities/UserEntity.cs
@@ -8,7 +8,7 @@ public class UserEntity
     [Key]
     public int Id { get; set; }
 
-    public DateTime Created {  get; set; }
+    public DateTime Created { get; set; }
     public DateTime LastModified { get; set; }
     public bool IsEnabled { get; set; }
 
