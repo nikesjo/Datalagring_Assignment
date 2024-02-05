41c7994ce348db51d5bb882e7709a1294aa4a976
Nike <nikesjolin@outlook.com>
2024-02-05 14:43:20 +0100

    Created ProductService and dtos

---
+++ Infrastructure/Dtos/ProductDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Infrastructure.Dtos;
+
+public class ProductDto
+{
+    public string ArticleNumber { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public string? Description { get; set; }
+    public string? Specification { get; set; }
+    public string Manufacture { get; set; } = null!;
+    public string CategoryName { get; set; } = null!;
+    public decimal? Price { get; set; }
+    public string? CurrencyCode { get; set; }
+}
---
+++ Infrastructure/Interfaces/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.Dtos;
+using Infrastructure.Entities;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Interfaces;
+
+public interface IProductService
+{
+    Task<ProductDto> CreateProductAsync(ProductRegDto productRegDto);
+    Task<bool> DeleteProductAsync(ProductDto productDto);
+    Task<ProductDto> GetUserAsync(Expression<Func<Product, bool>> expression);
+    Task<IEnumerable<ProductDto>> GetUsersAsync();
+    Task<ProductDto> UpdateUserAsync(ProductDto productDto);
+}
