f98657eb26b8612eabe81cb8318fc89ece5b27ee
Nike <nikesjolin@outlook.com>
2024-02-08 16:56:19 +0100

    Services tests done

---
+++ Infrastructure.Tests/Services/ProductService_Tests.cs
@@ -0,0 +1,230 @@
+﻿using Infrastructure.Contexts;
+using Infrastructure.Dtos;
+using Infrastructure.Entities;
+using Infrastructure.Interfaces;
+using Infrastructure.Repositories;
+using Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Tests.Services;
+
+public class ProductService_Tests
+{
+    private readonly ProductCatalogContext _productCatalogContext =
+        new(new DbContextOptionsBuilder<ProductCatalogContext>()
+            .UseInMemoryDatabase($"{Guid.NewGuid()}")
+            .Options);
+
+    [Fact]
+    public async Task CreateProductAsync_Should_CreateANewProduct_And_ReturnTrue()
+    {
+        // Arrange
+        IProductRepository productRepository = new ProductRepository(_productCatalogContext);
+        IManufactureRepository manufactureRepository = new ManufactureRepository(_productCatalogContext);
+        ICategoryRepository categoryRepository = new CategoryRepository(_productCatalogContext);
+        IProductPriceRepository productPriceRepository = new ProductPriceRepository(_productCatalogContext);
+        ICurrencyRepository currencyRepository = new CurrencyRepository(_productCatalogContext);
+        IProductService productService = new ProductService(categoryRepository, currencyRepository, manufactureRepository, productPriceRepository, productRepository);
+        var productRegDto = new ProductRegDto
+        {
+            ArticleNumber = "1",
+            Title = "Test",
+            Description = "Test",
+            Specification = "Test",
+            Manufacture = "Test",
+            CategoryName = "Test",
+            Price = 100,
+            CurrencyCode = "Test",
+            Currency = "Test"
+        };
+
+        // Act
+        var result = await productService.CreateProductAsync(productRegDto);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task GetProductsAsync_Should_GetAll_And_ReturnIEnumerableOfTypeProductDto()
+    {
+        // Arrange
+        IProductRepository productRepository = new ProductRepository(_productCatalogContext);
+        IManufactureRepository manufactureRepository = new ManufactureRepository(_productCatalogContext);
+        ICategoryRepository categoryRepository = new CategoryRepository(_productCatalogContext);
+        IProductPriceRepository productPriceRepository = new ProductPriceRepository(_productCatalogContext);
+        ICurrencyRepository currencyRepository = new CurrencyRepository(_productCatalogContext);
+        IProductService productService = new ProductService(categoryRepository, currencyRepository, manufactureRepository, productPriceRepository, productRepository);
+        var productRegDto = new ProductRegDto
+        {
+            ArticleNumber = "1",
+            Title = "Test",
+            Description = "Test",
+            Specification = "Test",
+            Manufacture = "Test",
+            CategoryName = "Test",
+            Price = 100,
+            CurrencyCode = "Test",
+            Currency = "Test"
+        };
+        await productService.CreateProductAsync(productRegDto);
+
+        // Act
+        var result = await productService.GetProductsAsync();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsAssignableFrom<IEnumerable<ProductDto>>(result);
+        Assert.Single(result);
+    }
+
+    [Fact]
+    public async Task GetProductAsync_Should_GetOneProduct_And_ReturnOneProductDto()
+    {
+        // Arrange
+        IProductRepository productRepository = new ProductRepository(_productCatalogContext);
+        IManufactureRepository manufactureRepository = new ManufactureRepository(_productCatalogContext);
+        ICategoryRepository categoryRepository = new CategoryRepository(_productCatalogContext);
+        IProductPriceRepository productPriceRepository = new ProductPriceRepository(_productCatalogContext);
+        ICurrencyRepository currencyRepository = new CurrencyRepository(_productCatalogContext);
+        IProductService productService = new ProductService(categoryRepository, currencyRepository, manufactureRepository, productPriceRepository, productRepository);
+        var productRegDto = new ProductRegDto
+        {
+            ArticleNumber = "1",
+            Title = "Test",
+            Description = "Test",
+            Specification = "Test",
+            Manufacture = "Test",
+            CategoryName = "Test",
+            Price = 100,
+            CurrencyCode = "Test",
+            Currency = "Test"
+        };
+        await productService.CreateProductAsync(productRegDto);
+
+        var productDto = new ProductDto
+        {
+            ArticleNumber = productRegDto.ArticleNumber,
+            Title = productRegDto.Title,
+            Description = productRegDto.Description,
+            Specification = productRegDto.Specification,
+            Manufacture = productRegDto.Manufacture,
+            CategoryName = productRegDto.CategoryName,
+            Price = productRegDto.Price,
+            CurrencyCode = productRegDto.CurrencyCode,
+            Currency = productRegDto.Currency
+        };
+
+        // Act
+        var result = await productService.GetProductAsync(x => x.ArticleNumber == productDto.ArticleNumber);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(productDto.ArticleNumber, result.ArticleNumber);
+    }
+
+    [Fact]
+    public async Task UpdateProductAsync_Should_UpdateExistingProduct_And_ReturnUpdatedProductDto()
+    {
+        // Arrange
+        IProductRepository productRepository = new ProductRepository(_productCatalogContext);
+        IManufactureRepository manufactureRepository = new ManufactureRepository(_productCatalogContext);
+        ICategoryRepository categoryRepository = new CategoryRepository(_productCatalogContext);
+        IProductPriceRepository productPriceRepository = new ProductPriceRepository(_productCatalogContext);
+        ICurrencyRepository currencyRepository = new CurrencyRepository(_productCatalogContext);
+        IProductService productService = new ProductService(categoryRepository, currencyRepository, manufactureRepository, productPriceRepository, productRepository);
+        var productRegDto = new ProductRegDto
+        {
+            ArticleNumber = "1",
+            Title = "Test",
+            Description = "Test",
+            Specification = "Test",
+            Manufacture = "Test",
+            CategoryName = "Test",
+            Price = 100,
+            CurrencyCode = "Test",
+            Currency = "Test"
+        };
+        await productService.CreateProductAsync(productRegDto);
+
+        var productDto = new ProductDto
+        {
+            ArticleNumber = productRegDto.ArticleNumber,
+            Title = productRegDto.Title,
+            Description = productRegDto.Description,
+            Specification = productRegDto.Specification,
+            Manufacture = productRegDto.Manufacture,
+            CategoryName = productRegDto.CategoryName,
+            Price = productRegDto.Price,
+            CurrencyCode = productRegDto.CurrencyCode,
+            Currency = productRegDto.Currency
+        };
+
+        // Act
+        productDto.Title = "NewTest";
+        productDto.Description = "NewTest";
+        productDto.Specification = "NewTest";
+        productDto.Manufacture = "NewTest";
+        productDto.CategoryName = "NewTest";
+        productDto.Price = 200;
+        productDto.CurrencyCode = "NewTest";
+        productDto.Currency = "NewTest";
+        var result = await productService.UpdateProductAsync(productDto);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(productDto.ArticleNumber, result.ArticleNumber);
+        Assert.Equal("NewTest", result.Title);
+        Assert.Equal("NewTest", result.Description);
+        Assert.Equal("NewTest", result.Specification);
+        Assert.Equal("NewTest", result.Manufacture);
+        Assert.Equal("NewTest", result.CategoryName);
+        Assert.Equal(200, result.Price);
+        Assert.Equal("NewTest", result.CurrencyCode);
+        Assert.Equal("NewTest", result.Currency);
+    }
+
+    [Fact]
+    public async Task DeleteProductAsync_Should_RemoveOneProduct_And_ReturnTrue()
+    {
+        // Arrange
+        IProductRepository productRepository = new ProductRepository(_productCatalogContext);
+        IManufactureRepository manufactureRepository = new ManufactureRepository(_productCatalogContext);
+        ICategoryRepository categoryRepository = new CategoryRepository(_productCatalogContext);
+        IProductPriceRepository productPriceRepository = new ProductPriceRepository(_productCatalogContext);
+        ICurrencyRepository currencyRepository = new CurrencyRepository(_productCatalogContext);
+        IProductService productService = new ProductService(categoryRepository, currencyRepository, manufactureRepository, productPriceRepository, productRepository);
+        var productRegDto = new ProductRegDto
+        {
+            ArticleNumber = "1",
+            Title = "Test",
+            Description = "Test",
+            Specification = "Test",
+            Manufacture = "Test",
+            CategoryName = "Test",
+            Price = 100,
+            CurrencyCode = "Test",
+            Currency = "Test"
+        };
+        await productService.CreateProductAsync(productRegDto);
+
+        var productDto = new ProductDto
+        {
+            ArticleNumber = productRegDto.ArticleNumber,
+            Title = productRegDto.Title,
+            Description = productRegDto.Description,
+            Specification = productRegDto.Specification,
+            Manufacture = productRegDto.Manufacture,
+            CategoryName = productRegDto.CategoryName,
+            Price = productRegDto.Price,
+            CurrencyCode = productRegDto.CurrencyCode,
+            Currency = productRegDto.Currency
+        };
+
+        // Act
+        var result = await productService.DeleteProductAsync(productDto);
+
+        // Assert
+        Assert.True(result);
+    }
+}
---
+++ Infrastructure.Tests/Services/UserService_Tests.cs
@@ -0,0 +1,247 @@
+﻿using Infrastructure.Contexts;
+using Infrastructure.Dtos;
+using Infrastructure.Entities;
+using Infrastructure.Interfaces;
+using Infrastructure.Repositories;
+using Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Tests.Services;
+
+public class UserService_Tests
+{
+    private readonly UserContext _userContext =
+        new(new DbContextOptionsBuilder<UserContext>()
+            .UseInMemoryDatabase($"{Guid.NewGuid()}")
+            .Options);
+
+    [Fact]
+    public async Task CreateUserAsync_Should_CreateANewUser_And_ReturnUserDto()
+    {
+        // Arrange
+        IUserRepository userRepository = new UserRepository(_userContext);
+        IAuthRepository authRepository = new AuthRepository(_userContext);
+        IProfileRepository profileRepository = new ProfileRepository(_userContext);
+        IAddressRepository addressRepository = new AddressRepository(_userContext);
+        IUserService userService = new UserService(userRepository, authRepository, profileRepository, addressRepository);
+        var userRegistrationDto = new UserRegistrationDto
+        {
+            FirstName = "Test",
+            LastName = "Test",
+            PhoneNumber = "Test",
+            Email = "Test",
+            Password = "Test",
+            StreetName = "Test",
+            PostalCode = "Test",
+            City = "Test"
+        };
+
+        // Act
+        var result = await userService.CreateUserAsync(userRegistrationDto);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(1, result.Id);
+    }
+
+    [Fact]
+    public async Task GetUsersAsync_Should_GetAllUsers_And_ReturnIEnumerableOfTypeUserDto()
+    {
+        // Arrange
+        IUserRepository userRepository = new UserRepository(_userContext);
+        IAuthRepository authRepository = new AuthRepository(_userContext);
+        IProfileRepository profileRepository = new ProfileRepository(_userContext);
+        IAddressRepository addressRepository = new AddressRepository(_userContext);
+        IUserService userService = new UserService(userRepository, authRepository, profileRepository, addressRepository);
+        var userRegistrationDto = new UserRegistrationDto
+        {
+            FirstName = "Test",
+            LastName = "Test",
+            PhoneNumber = "Test",
+            Email = "Test",
+            Password = "Test",
+            StreetName = "Test",
+            PostalCode = "Test",
+            City = "Test"
+        };
+        await userService.CreateUserAsync(userRegistrationDto);
+
+        // Act
+        var result = await userService.GetUsersAsync();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsAssignableFrom<IEnumerable<UserDto>>(result);
+        Assert.Single(result);
+    }
+
+    [Fact]
+    public async Task GetUserAsync_Should_GetOneUser_And_ReturnOneUserDto()
+    {
+        // Arrange
+        IUserRepository userRepository = new UserRepository(_userContext);
+        IAuthRepository authRepository = new AuthRepository(_userContext);
+        IProfileRepository profileRepository = new ProfileRepository(_userContext);
+        IAddressRepository addressRepository = new AddressRepository(_userContext);
+        IUserService userService = new UserService(userRepository, authRepository, profileRepository, addressRepository);
+        var userRegistrationDto = new UserRegistrationDto
+        {
+            FirstName = "Test",
+            LastName = "Test",
+            PhoneNumber = "Test",
+            Email = "Test",
+            Password = "Test",
+            StreetName = "Test",
+            PostalCode = "Test",
+            City = "Test"
+        };
+        await userService.CreateUserAsync(userRegistrationDto);
+
+        var userDto = new UserDto
+        {
+            Id = 1,
+            FirstName = userRegistrationDto.FirstName,
+            LastName = userRegistrationDto.LastName,
+            PhoneNumber = userRegistrationDto.PhoneNumber,
+            Email = userRegistrationDto.PhoneNumber,
+            Password = userRegistrationDto.Password,
+            LastModified = DateTime.Now,
+            Addresses = new List<AddressDto>
+            {
+                new AddressDto
+                {
+                    StreetName = userRegistrationDto.StreetName,
+                    PostalCode = userRegistrationDto.PostalCode,
+                    City = userRegistrationDto.City
+                }
+            }
+        };
+
+        // Act
+        var result = await userService.GetUserAsync(x => x.Id == userDto.Id);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(userDto.Id, result.Id);
+    }
+
+    [Fact]
+    public async Task UpdateUserAsync_Should_UpdateExistingUser_And_ReturnUpdatedUserDto()
+    {
+        // Arrange
+        IUserRepository userRepository = new UserRepository(_userContext);
+        IAuthRepository authRepository = new AuthRepository(_userContext);
+        IProfileRepository profileRepository = new ProfileRepository(_userContext);
+        IAddressRepository addressRepository = new AddressRepository(_userContext);
+        IUserService userService = new UserService(userRepository, authRepository, profileRepository, addressRepository);
+        var userRegistrationDto = new UserRegistrationDto
+        {
+            FirstName = "Test",
+            LastName = "Test",
+            PhoneNumber = "Test",
+            Email = "Test",
+            Password = "Test",
+            StreetName = "Test",
+            PostalCode = "Test",
+            City = "Test"
+        };
+        await userService.CreateUserAsync(userRegistrationDto);
+
+        var userDto = new UserDto
+        {
+            Id = 1,
+            FirstName = userRegistrationDto.FirstName,
+            LastName = userRegistrationDto.LastName,
+            PhoneNumber = userRegistrationDto.PhoneNumber,
+            Email = userRegistrationDto.PhoneNumber,
+            Password = userRegistrationDto.Password,
+            LastModified = DateTime.Now,
+            Addresses = new List<AddressDto>
+            {
+                new AddressDto
+                {
+                    StreetName = userRegistrationDto.StreetName,
+                    PostalCode = userRegistrationDto.PostalCode,
+                    City = userRegistrationDto.City
+                }
+            }
+        };
+
+        // Act
+        userDto.FirstName = "NewTest";
+        userDto.LastName = "NewTest";
+        userDto.PhoneNumber = "NewTest";
+        userDto.Email = "NewTest";
+        userDto.Password = "NewTest";
+        userDto.LastModified = DateTime.Today;
+        userDto.Addresses = new List<AddressDto>
+        {
+            new AddressDto
+            {
+                StreetName = "NewTest",
+                PostalCode = "NewTest",
+                City = "NewTest"
+            }
+        };
+        var result = await userService.UpdateUserAsync(userDto);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(userDto.Id, result.Id);
+        Assert.Equal("NewTest", result.FirstName);
+        Assert.Equal("NewTest", result.LastName);
+        Assert.Equal("NewTest", result.PhoneNumber);
+        Assert.Equal("NewTest", result.Email);
+        Assert.Equal("NewTest", result.Password);
+        Assert.Equal(DateTime.Today, result.LastModified);
+    }
+
+    [Fact]
+    public async Task DeleteUserAsync_Should_RemoveOneUser_And_ReturnTrue()
+    {
+        // Arrange
+        IUserRepository userRepository = new UserRepository(_userContext);
+        IAuthRepository authRepository = new AuthRepository(_userContext);
+        IProfileRepository profileRepository = new ProfileRepository(_userContext);
+        IAddressRepository addressRepository = new AddressRepository(_userContext);
+        IUserService userService = new UserService(userRepository, authRepository, profileRepository, addressRepository);
+        var userRegistrationDto = new UserRegistrationDto
+        {
+            FirstName = "Test",
+            LastName = "Test",
+            PhoneNumber = "Test",
+            Email = "Test",
+            Password = "Test",
+            StreetName = "Test",
+            PostalCode = "Test",
+            City = "Test"
+        };
+        await userService.CreateUserAsync(userRegistrationDto);
+
+        var userDto = new UserDto
+        {
+            Id = 1,
+            FirstName = userRegistrationDto.FirstName,
+            LastName = userRegistrationDto.LastName,
+            PhoneNumber = userRegistrationDto.PhoneNumber,
+            Email = userRegistrationDto.PhoneNumber,
+            Password = userRegistrationDto.Password,
+            LastModified = DateTime.Now,
+            Addresses = new List<AddressDto>
+            {
+                new AddressDto
+                {
+                    StreetName = userRegistrationDto.StreetName,
+                    PostalCode = userRegistrationDto.PostalCode,
+                    City = userRegistrationDto.City
+                }
+            }
+        };
+
+        // Act
+        var result = await userService.DeleteUserAsync(userDto);
+
+        // Assert
+        Assert.True(result);
+    }
+}
